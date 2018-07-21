59c8e1b25d1600180bda11b5307ebdc8cefe67e8
Michael Pope <popemray1986@gmail.com>
2018-07-21 18:00:29 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180721174034) do
+ActiveRecord::Schema.define(version: 20180721175959) do
+
+  create_table "artist_songs", force: :cascade do |t|
+    t.integer "artist_id"
+    t.integer "song_id"
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "author_songs", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "song_id"
   end
 
   create_table "songs", force: :cascade do |t|
